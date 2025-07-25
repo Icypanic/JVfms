2a91ba113da306ddc7f0c9554fa0c2d9eac3a225
Icypanic <loganblakey10alt@gmail.com>
2025-07-25 06:43:50 +0100

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1f2937', // Dark theme base
+        accent: '#eab308'   // Yellowish JYFMS highlight
+      }
+    },
+  },
+  plugins: [],
+};
