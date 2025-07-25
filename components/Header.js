b5f4254375399149beb7ce6d1f3cde4a10939713
Icypanic <loganblakey10alt@gmail.com>
2025-07-25 06:45:52 +0100

    Header.js

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-4 bg-black shadow-lg">
+      <div className="text-xl font-bold">JYFMS</div>
+      <div className="text-sm text-gray-400">Player: Logan</div>
+    </header>
+  );
+}
